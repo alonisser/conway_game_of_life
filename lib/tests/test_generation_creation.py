0f33c9d0440fc30d5cb4615c2f6c9b9d0d0cfaf7
alonisser <alonisser@gmail.com>
2020-04-27 21:07:23 +0300

    first failing test

--- lib/tests/test_generation_creation.py
+++ lib/tests/test_generation_creation.py
@@ -1,9 +1,18 @@
 import unittest
+import numpy as np
+from numpy.testing import assert_array_equal
+
+from lib.generation import resolve_next_generation
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class GenerationCreatorTestCase(unittest.TestCase):
+    def test_all_dead_generation_resolves_to_all_dead_generation(self):
+        original_generation = np.array([[0, 0, 0, 0],
+                                        [0, 0, 0, 0],
+                                        [0, 0, 0, 0]])
+
+        new_generation = resolve_next_generation(original_generation)
+        assert_array_equal(original_generation, new_generation)
 
 
 if __name__ == '__main__':

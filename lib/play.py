264c71a83fe46a53c651542633b87fc4d18d75f7
alonisser <alonisser@gmail.com>
2020-04-29 00:49:30 +0300

    'better' UI

--- lib/play.py
+++ lib/play.py
@@ -1,3 +1,4 @@
+import curses
 import time
 from curses import wrapper
 
@@ -15,7 +16,7 @@ def play_life(stdscr, new_generation):
     is_alive = True
 
     while is_alive:
-        time.sleep(2)
+        time.sleep(1)
         new_generation = resolve_next_generation(new_generation)
         title = build_title(new_generation, rounds)
         stdscr.clear()
@@ -26,8 +27,12 @@ def play_life(stdscr, new_generation):
         stdscr.refresh()
         is_alive = new_generation.sum() > 0
         rounds = rounds + 1
-    stdscr.addstr("Game over, everything that has a beginning has an end you've played %s rounds" % rounds)
+    stdscr.addstr("\n Game over, everything that has a beginning has an end. You've played %s rounds" % rounds,
+                  curses.A_BLINK)
     stdscr.refresh()
+
+    stdscr.addstr("\n \n Exit with any key", curses.A_BOLD)
+    stdscr.getch()
 
 
 def build_title(new_generation, rounds):
